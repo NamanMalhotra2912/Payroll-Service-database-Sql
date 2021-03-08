86cfa218a31c18f347a5c2c3b9c18f08cdb99795
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-08 23:02:51 +0530

    Payroll service data using MySql

--- Address Book.sql
+++ Address Book.sql
@@ -2,7 +2,43 @@
 show databases;
 create database Address_Book;
 use Address_Book;
--- UC2
 
-create table address_book
+-- UC2
+CREATE TABLE address_book (
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    zip VARCHAR(50),
+    phone_number VARCHAR(10)
+);
+Alter table address_book add email varchar(50) not null after phone_number;
+select * from Address_Book.address_book;
+
+-- UC3
+insert into address_book(first_name, last_name,city,state,zip,phone_number,email) values ('Neeraj','Malhotra','Gurgaon','Haryana','122001','8588801333','neeraj@gmail.com');
+insert into address_book(first_name, last_name,city,state,zip,phone_number,email) values ('Raj','Malhotra','Gurgaon','Haryana','122001','8588801333','neeraj@gmail.com');
+select * from Address_Book.address_book;
+
+-- UC4
+update address_book set first_name = 'Naman' where first_name = 'Raj';
+select * from Address_Book.address_book;
+
+-- UC5
+delete from address_book where first_name = 'Naman';
+select * from Address_Book.address_book;
+
+-- UC6
+select first_name from address_book where city = 'Gurgaon';
+
+-- UC7
+select count(city) from address_book;
+select count(state) from address_book;
+
+-- UC8
+insert into address_book(first_name, last_name,city,state,zip,phone_number,email) values ('Raj','Malhotra','Gurgaon','Haryana','122001','8588801333','neeraj@gmail.com');
+insert into address_book(first_name, last_name,city,state,zip,phone_number,email) values ('Nitin','Kumar','Delhi','Haryana','122001','1234567890','xyz@gmail.com');
+select * from Address_Book.address_book;
+select * from address_book where city = 'Gurgaon' order by first_name ;
+
 
